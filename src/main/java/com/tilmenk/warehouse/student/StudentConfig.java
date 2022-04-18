51edde3fbaa4bf787eedbc27da0c868bbb3cdff4
Henk <henk.vandrsloot@gmail.com>
2022-04-19 00:56:52 +0200

    add prod/dev env handling

--- src/main/java/com/tilmenk/warehouse/student/StudentConfig.java
+++ src/main/java/com/tilmenk/warehouse/student/StudentConfig.java
@@ -14,16 +14,13 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
-            Student mariam = new Student(
-                    "Mariam",
-                    "mariam.jamal@gmail.com",
+            Student mariam = new Student("Mariam", "mariam.jamal@gmail.com",
                     LocalDate.of(2000, Month.APRIL, 22));
-            Student alex = new Student(
-                    "Alex",
-                    "alex.jamal@gmail.com",
+            Student alex = new Student("Alex", "alex.jamal@gmail.com",
                     LocalDate.of(2004, Month.APRIL, 22));
-
-            repository.saveAll(List.of(mariam, alex));
+            if (!System.getenv("DEPLOYMENT_ENV").equals("dev")) {
+                repository.saveAll(List.of(mariam, alex));
+            }
         };
     }
 }
