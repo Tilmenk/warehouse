36cdfeae0993ffd64a357aa79143cdc6ef2c81df
Henk <henk.vandrsloot@gmail.com>
2022-04-19 00:22:23 +0200

    testing out remote db

--- src/main/java/com/tilmenk/warehouse/WarehouseApplication.java
+++ src/main/java/com/tilmenk/warehouse/WarehouseApplication.java
@@ -2,19 +2,16 @@ package com.tilmenk.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+//@PropertySource("classpath:application-${spring.profiles.active:default}
+// .properties")
 public class WarehouseApplication {
-    private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        applicationContext = SpringApplication.run(WarehouseApplication.class
-                , args);
-        System.out.println(applicationContext.containsBean("Student"));
-
+        SpringApplication.run(WarehouseApplication.class, args);
     }
 
 }
