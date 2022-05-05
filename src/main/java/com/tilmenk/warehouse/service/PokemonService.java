1a5f05be34748326549702965315326207f5c4ca
Henk <henk.vandrsloot@gmail.com>
2022-05-05 15:58:40 +0200

    dont throw just log and skip

--- src/main/java/com/tilmenk/warehouse/service/PokemonService.java
+++ src/main/java/com/tilmenk/warehouse/service/PokemonService.java
@@ -3,12 +3,14 @@ package com.tilmenk.warehouse.service;
 import com.tilmenk.warehouse.exceptions.PokemonAlreadyInDbException;
 import com.tilmenk.warehouse.model.Pokemon;
 import com.tilmenk.warehouse.repository.PokemonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class PokemonService {
     private final PokemonRepository pokemonRepository;
 
@@ -23,11 +25,11 @@ public class PokemonService {
 
     public boolean savePokemon(Pokemon pokemon) {
         if (pokemonRepository.findPokemonByName(pokemon.getName()).isPresent()) {
-            throw new PokemonAlreadyInDbException(pokemon);
+            log.error(new PokemonAlreadyInDbException(pokemon).getMessage());
+            return false;
         } else {
             pokemonRepository.save(pokemon);
             return true;
         }
     }
-
 }
